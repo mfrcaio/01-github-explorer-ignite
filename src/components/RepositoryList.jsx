e5af042b032b596e7bf7051b523cf8a6e383b995
Caio Marques <mfr.caio@gmail.com>
2021-03-10 09:39:56 -0300

    Incluindo lista de Repositorios

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+const RepositoryName = "unform2";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositório</h1>
+
+      <ul>
+        <li>
+          <strong>{RepositoryName}</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
